840083ab97051117cb5ee406e66a426e7754aa9e
Eduardo Medina <eduardomedinaom@hotmail.com>
2020-12-11 13:26:18 -0700

    Initial commit

---
+++ app/src/main/java/com/example/eva2_4_extras/DetailActivity.java
@@ -0,0 +1,23 @@
+package com.example.eva2_4_extras;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class DetailActivity extends AppCompatActivity {
+
+    TextView txtVwDatos;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+        txtVwDatos = findViewById(R.id.txtVwDatos);
+        Intent intent = getIntent();
+        txtVwDatos.append(intent.getStringExtra("MENSAJE"));
+        txtVwDatos.append("" + intent.getIntExtra("ENTERO",0));
+        txtVwDatos.append("" + intent.getFloatExtra("PRECIO",0.0f));
+
+    }
+}
